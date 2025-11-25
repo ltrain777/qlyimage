5960947f9968bb5280c1b620295c79937779baaf
LTrain Huynh <ltrain721@gmail.com>
2025-11-24 19:19:25 -0800

    install qlydbg.debon  qly-client image

--- Readme.md
+++ Readme.md
@@ -29,6 +29,14 @@ docker build -t qly-client:v2 -f Dockerfile-qlyclient .
 xhost +local:docker
 ```
 
+#### Ensure `iw` is installed to identify the wireless card's ip address
 ```bash
-docker run -it --rm -e ROS_HOSTNAME="192.168.68.70" -e DISPLAY=$DISPLAY --volume="/tmp/.X11-unix:/tmp/.X11-unit:rw" --volume="/usr/lib/.../libGL.so.1:/usr/lib/.../libGL.so.1:ro" --device=/dev/dri:/dev/dri --net=host qly-client:v2 /bin/bash
+sudo apt install iw
+```
+
+```bash
+ROSMASTER=http://<IPC_IP_Address>:11311
+HOSTIP=$(ip -4 addr show $(iw dev | awk '$1=="Interface"{print $2}') | grep -oP '(?<=inet\s)\d+(\.\d+){3}')
+
+docker run -it --rm -e ROS_HOSTNAME=$HOSTIP -e ROS_MASTER_URI=$ROSMASTER -e DISPLAY=$DISPLAY --volume="/tmp/.X11-unix:/tmp/.X11-unit:rw" --volume="/usr/lib/.../libGL.so.1:/usr/lib/.../libGL.so.1:ro" --device=/dev/dri:/dev/dri --net=host qly-client:v4 /bin/bash
 ```
--- entrypoint_qlyclient.sh
+++ entrypoint_qlyclient.sh
@@ -4,14 +4,14 @@ set -e
 # 1. Source standard ROS Noetic environment
 source /opt/ros/noetic/setup.bash
 
-# # 2. Source QLY proprietary debug environment
-# # This step is critical as it sets paths needed for 'qly_debug_rviz' [4-6].
-# if [ -f "/opt/qlybot/setup.bash" ]; then
-#     source /opt/qlybot/setup.bash
-#     echo "QLY debug environment sourced."
-# else
-#     echo "Warning: /opt/qlybot/setup.bash not found. Cannot guarantee launch functionality."
-# fi
+# 2. Source QLY proprietary debug environment
+# This step is critical as it sets paths needed for 'qly_debug_rviz' [4-6].
+ if [ -f "/opt/qlydbg/setup.bash" ]; then
+     source /opt/qlydbg/setup.bash
+     echo "QLY debug environment sourced."
+ else
+     echo "Warning: /opt/qlydbg/setup.bash not found. Cannot guarantee launch functionality."
+ fi
 
 # 3. ROS_HOSTNAME must be dynamically set at runtime via the 'docker run' command
 # The user must provide the laptop's IP address on the QLY network (e.g., 192.168.254.100).
@@ -19,6 +19,13 @@ if [ -z "$ROS_HOSTNAME" ]; then
     echo "FATAL: ROS_HOSTNAME environment variable is not set. Cannot establish communication."
     exit 1
 fi
+
+# 4. ROS_MASTER_URI needs to point to the QLY robot's ROS master.
+# The user must provide the robot's IP address on the QLY network (e.g.,
+if [ -z "$ROS_MASTER_URI" ]; then
+    echo "FATAL: ROS_MASTER_URI environment variable is not set. Cannot establish communication."
+    exit 1
+fi  
 
 # Execute the command passed to the container (e.g., roslaunch qly_debug_rviz nav_rviz.launch)
 exec "$@"
